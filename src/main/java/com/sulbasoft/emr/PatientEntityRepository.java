b3374b6f2fb76e4dd6f23bad9a08a55a2ece6909
Muhammad S. Ali <salisu14@yahoo.com>
2023-10-05 22:52:00 +0100

    Add @Repository annotation

--- src/main/java/com/sulbasoft/emr/PatientEntityRepository.java
+++ src/main/java/com/sulbasoft/emr/PatientEntityRepository.java
@@ -1,7 +1,9 @@
 package com.sulbasoft.emr;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PatientEntityRepository extends CrudRepository<PatientEntity, Long> {
     
 }
