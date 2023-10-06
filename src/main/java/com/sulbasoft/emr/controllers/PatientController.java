4add9318b806e15be0b68d13d9051acc9b949227
Muhammad S. Ali <salisu14@yahoo.com>
2023-10-06 20:54:38 +0100

    Add a method for searching patients.

--- src/main/java/com/sulbasoft/emr/controllers/PatientController.java
+++ src/main/java/com/sulbasoft/emr/controllers/PatientController.java
@@ -1,7 +1,10 @@
 package com.sulbasoft.emr.controllers;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,5 +27,22 @@ public class PatientController {
     Iterable<PatientEntity> all() {
         System.out.println( repository.findAll());
         return repository.findAll();
+    }
+
+    @GetMapping("/search")
+    public List<PatientEntity> search(PatientEntity patientEntity) {
+        if(StringUtils.hasText(patientEntity.getFirstName()) && StringUtils.hasText(patientEntity.getLastName())) {
+            return repository.findByFirstNameContainsOrLastNameContainsAllIgnoreCase(patientEntity.getFirstName(), patientEntity.getLastName());
+        }
+
+        if (StringUtils.hasText(patientEntity.getFirstName())) {
+            return repository.findByFirstNameContainsIgnoreCase(patientEntity.getFirstName());
+        }
+
+        if (StringUtils.hasText(patientEntity.getFirstName())) {
+            return repository.findByLastNameContainsIgnoreCase(patientEntity.getFirstName());
+        }
+
+        return null;
     }
 }
